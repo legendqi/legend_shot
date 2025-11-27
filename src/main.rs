1d975855b71906eff7c6cb4b785855eb0b369701
yuwenqi <yuwenqi9@flkj.ai>
2025-11-27 11:17:20 +0800

    1，优化代码

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,10 @@ mod app_handle;
 mod app;
 
 fn main() -> eframe::Result<()> {
-
-    // 在 Windows 上禁用 DPI 缩放感知
-    #[cfg(target_os = "windows")]
-    set_dpi_awareness();
-    
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_fullscreen(true)
-            .with_maximized(true)
+            // .with_maximized(true)
             .with_decorations(false)
             .with_always_on_top()
             .with_maximize_button(false)
@@ -36,19 +31,4 @@ fn main() -> eframe::Result<()> {
 
     Ok(())
 }
-
-#[cfg(target_os = "windows")]
-fn set_dpi_awareness() {
-    use winapi::um::shellscalingapi::{SetProcessDpiAwareness, PROCESS_PER_MONITOR_DPI_AWARE};
-    use winapi::um::winuser::SetProcessDPIAware;
-
-    // 尝试使用较新的 API（Windows 10+）
-    let result = unsafe { SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE) };
-
-    // 如果失败，回退到旧版 API
-    if result != 0 {
-        unsafe { SetProcessDPIAware() };
-    }
-}
-
 
